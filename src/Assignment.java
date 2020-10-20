4fad2d2a1a404c7667c9d15b4647c67a53f9ec73
tunnellm <70550370+tunnellm@users.noreply.github.com>
2020-10-20 07:43:03 -0400

    Revert "Created a main test bench"
    This reverts commit b9c03aa601ead362512241d394161a847f644be1.

--- src/Assignment.java
+++ src/Assignment.java
@@ -54,25 +54,7 @@ public class Assignment {
     }
 
     public static void main(String[] args){
-        System.out.println("---Testing Current Methods---");
-        Assignment a0 = new Assignment();
-        System.out.println("Assignment 0 (Default): " + a0.getInfo());
-        Assignment a1 = new Assignment(100.00,"low",true);
-        System.out.println("Assignment 1: " + a1.getInfo());
-        System.out.println("Showing gets for Default:");
-        System.out.println(a0.getGrade());
-        System.out.println(a0.getPriority());
-        System.out.println(a0.getComplete());
-        System.out.println("\nSetting Default equal to Assignment 1...");
-        a0.setGrade(100.00);
-        a0.setPriority("low");
-        a0.setComplete(true);
-        if (a0.getInfo().equals(a1.getInfo())) {
-            System.out.println("Correct! Results:");
-        }else {
-            System.out.println("Error");
-        }
-        System.out.println(a0.getInfo() + "\n" + a1.getInfo());
-        System.out.println("--Test Complete--");
+        Assignment a = new Assignment(100.00,"low",true);
+        System.out.println(a.getInfo());
     }
 }
