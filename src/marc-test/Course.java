f91a38932e03190302377c26c2efba87b721a5b8
tunnellm <70550370+tunnellm@users.noreply.github.com>
2020-10-20 12:42:30 -0400

    Course, ExitService, LoadCourseCatalog commit
    Fixed mistake made on github and class comits

---
+++ src/marc-test/Course.java
@@ -0,0 +1,47 @@
+package com.example.colcalendar18;
+// import assignment
+// import grade-scale
+import java.util.*;
+
+/*
+*
+* */
+
+public class Course<GradeScale, Assignment> {
+
+
+    String courseName;
+    int creditHours;
+    ArrayList<Assignment> assignments = new ArrayList();
+    ArrayList<GradeScale> gradeScale = new ArrayList();
+
+    public Course(String courseName, int creditHours) {
+        this.courseName = courseName;
+        this.creditHours = creditHours;
+    }
+
+    public int getCreditHours() {
+        return this.creditHours;
+    }
+
+    public void addAssignment(Assignment assignment, GradeScale gradeScale) {
+        this.assignments.add(assignment);
+        this.gradeScale.add(gradeScale);
+    }
+
+    public void setCreditHours(int hours) {
+        this.creditHours = hours;
+    }
+
+    public void setCourseName(String name) {
+        this.courseName = name;
+    }
+
+    public String getCourseName() {
+        return this.courseName;
+    }
+
+    public void assignmentsList() {
+        // TO-DO; Populate list with assignments and corresponding weighting
+    }
+}
---
+++ src/marc-test/ExitService.java
@@ -0,0 +1,28 @@
+package com.example.colcalendar18;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class ExitService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        // Code Snippet Credit https://stackoverflow.com/questions/47849726/do-something-when-app-is-closed
+        Log.d("sdsd", "Sdsd");
+        super.onTaskRemoved(rootIntent);
+
+        // We'll be adding code to save files and stuff here
+        //do something you want before app closes.
+        //stop service
+        this.stopSelf();
+    }
+}
