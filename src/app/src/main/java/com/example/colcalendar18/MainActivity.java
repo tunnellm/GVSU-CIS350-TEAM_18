f2cfefbde78dff71a774dd3997d24837c4ff3527
tunnellm <70550370+tunnellm@users.noreply.github.com>
2020-10-20 07:46:55 -0400

    Revert "Course Class"
    This reverts commit 20f749f5a252368d6426f1a04e27b5e39f947c0f.

--- src/app/src/main/java/com/example/colcalendar18/MainActivity.java
+++ src/app/src/main/java/com/example/colcalendar18/MainActivity.java
@@ -2,12 +2,8 @@ package com.example.colcalendar18;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.CalendarView;
-
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,19 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Course course = new Course("CIS 350", 10);
         setContentView(R.layout.activity_main);
         calendar = (CalendarView) findViewById(R.id.calendar);
         calendar.setDate(calendar.getDate());
-
-
-
-
-//        File file = new File(getFilesDir(), "course.properties");
-//        Log.d("F", file.getAbsolutePath()); 
-//        Intent intent = new Intent(this, ExitService.class);
-//            startService(intent);
-
     }
 }
