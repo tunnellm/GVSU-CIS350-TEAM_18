e1d34b02355e1396f35f6d62f770a2f58d87a7f6
Jack Groothuis <groothuj@mail.gvsu.edu>
2020-11-22 17:20:12 -0500

    Added US Holidays to CalendarView

--- src/app/src/main/java/com/example/colcalendar18/MainActivity.java
+++ src/app/src/main/java/com/example/colcalendar18/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity
@@ -25,6 +26,68 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         calendar = (CalendarView) findViewById(R.id.calendar);
         calendar.setDate(calendar.getDate());
+
+        /* This overwritten method determines if the selected date is a
+         * US Holiday using switch/case and if statements to determine
+         * the correct date. (10 Total)
+         */
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                switch (month) {
+                    case 0:
+                        if (dayOfMonth == 1) {
+                            Toast.makeText(MainActivity.this, "New Year's Day", Toast.LENGTH_SHORT).show();
+                        }else if (dayOfMonth == 20) {
+                            Toast.makeText(MainActivity.this, "Martin Luther King, Jr. Day", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 1:
+                        if (dayOfMonth == 17) {
+                            Toast.makeText(MainActivity.this, "George Washington's Birthday", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 4:
+                        if (dayOfMonth == 25) {
+                            Toast.makeText(MainActivity.this, "Memorial Day", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 6:
+                        if (dayOfMonth == 4) {
+                            Toast.makeText(MainActivity.this, "Independence Day", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 8:
+                        if (dayOfMonth == 7) {
+                            Toast.makeText(MainActivity.this, "Labor Day", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 9:
+                        if (dayOfMonth == 12) {
+                            Toast.makeText(MainActivity.this, "Columbus Day", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 10:
+                        switch (dayOfMonth) {
+                            case 11:
+                                Toast.makeText(MainActivity.this, "Veterans Day", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 26:
+                                Toast.makeText(MainActivity.this, "Thanksgiving Day", Toast.LENGTH_SHORT).show();
+                            default:
+                                break;
+                        }
+                        break;
+                    case 11:
+                        if (dayOfMonth == 25) {
+                            Toast.makeText(MainActivity.this, "Christmas Day", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
 
         loadCourses = findViewById(R.id.managecourse);
         loadCourses.setOnClickListener(this);
